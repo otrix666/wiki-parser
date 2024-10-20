cfdf1d587e2faf9af2bba1529fcd760a493608c7
Jambo <otrix666.dut@gmail.com>
2024-10-20 13:02:05 +0300

    fix(main) fixed method of checking uploaded links

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import sqlite3
+
 from collections import deque
 
-from db import db
+from db import Database
 
 from parser import get_url_content
-from utils import fetch_ulrs_from_html_content
+from utils import create_str_list_uploaded_wiki_url, fetch_ulrs_from_html_content
 
 
-def parse_wikipedia_page(url: str, max_depth: int = 6) -> None:
+def parse_wikipedia_page(db: Database, url: str, max_depth: int = 6) -> None:
+    already_uploaded_urls = create_str_list_uploaded_wiki_url(db.get_urls())
+
     queue = deque([(url, 1)])
 
     while queue:
@@ -15,11 +19,12 @@ def parse_wikipedia_page(url: str, max_depth: int = 6) -> None:
         if current_depth > max_depth:
             continue
 
-        if db.get_url(url=current_url):
+        if current_url in already_uploaded_urls:
             continue
 
         db.add_url(url=current_url, depth=current_depth)
-        
+        already_uploaded_urls.append(current_url)
+
         html_content = get_url_content(url=current_url)
         if not html_content:
             continue
@@ -29,19 +34,21 @@ def parse_wikipedia_page(url: str, max_depth: int = 6) -> None:
         for next_url in urls:
             queue.append((next_url, current_depth + 1))
 
-        print(123)
-        print(321)
 
 def main():
-    url = input("Enter wiki URL for parsing: ")
-    parse_wikipedia_page(url=url)
+    try:
+        url = input("Enter wiki URL for parsing: ")
+
+        connection = sqlite3.connect("parser.db")
+        db = Database(connection=connection)
+
+        parse_wikipedia_page(db=db, url=url)
+    finally:
+        connection.close()
 
 
 if __name__ == "__main__":
     try:
-        db.create_table()
         main()
     except KeyboardInterrupt:
         print("wiki-cli stoped")
-    finally:
-        db._closer()
