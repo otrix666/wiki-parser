560bae352fabcf7aa2e2b1a1c2278b7faf8a3875
Jambo <otrix666.dut@gmail.com>
2024-10-20 12:59:22 +0300

    feat(utils) added new util for create list of uploaded urls

--- utils.py
+++ utils.py
@@ -4,3 +4,7 @@ import re
 def fetch_ulrs_from_html_content(html_content: str) -> set[str]:
     url_pattern = r'(https?://[^\s">]+)'
     return set(re.findall(url_pattern, html_content))
+
+
+def create_str_list_uploaded_wiki_url(uploaded_urls: list[tuple]):
+    return [url[0] for url in uploaded_urls]
