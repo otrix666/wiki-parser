3af19d7e4ff3e0996257234a7153ca2f67178325
Jambo <otrix666.dut@gmail.com>
2024-10-20 19:33:24 +0300

    refactor(utils) delete <create_str_list_uploaded_wiki_url>

--- utils.py
+++ utils.py
@@ -6,5 +6,3 @@ def fetch_ulrs_from_html_content(html_content: str) -> set[str]:
     return set(re.findall(url_pattern, html_content))
 
 
-def create_str_list_uploaded_wiki_url(uploaded_urls: list[tuple]):
-    return [url[0] for url in uploaded_urls]
